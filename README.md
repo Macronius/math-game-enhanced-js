ccee7abd705ed416867285882d106bfb149f2fa1
Marcus <marconiushenry@gmail.com>
2021-04-27 11:41:02 -0500

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+Simple math game, modified from a tutorial math project using JS.
+
+Modification took from only asking multiplication questions to: addition, subtraction, multiplication, division, exponentiation, and nth-root calculations.
+
+TODO:
+- rebuild using React
+- style needs work
+- increase 'container' to utilize more of the screen
+- local storage to track 'score history'
+- 'game over' screen with appropriate message, depending on how current score compares to historical
+- introduce playstyle adjustment options:
+    - difficulty
+        - larger numbers
+        - fractions
+        - decreased time
+        - more difficult concepts
+
+(all for the purpose of increasing users intuition for the correct answer, not actually solving for the exact solution)
+
+- could be interesting to have a Tetris style, where the question falls at a certain rate (according to level) and player must adjust its trajectory to land on the correct solution at the bottom.
+    NOTE: all rotation action that would otherwise happen to tetriminos in Tetris in fact will occur by the gears in the player's head by grinding out a solution; this absent feature would not diminish the captivation of player.
+
+- introduce periodic random, but inevitable, portals to another dimension of gameplay.  If the other three options are incorrect, then the correct answer must be the artifact from another dimension...[of game play] 
+    NOTE: I don't really know what that means
+
+- option to post achievement to facefook
+
---
+++ script.js
@@ -0,0 +1,196 @@
+//define variables
+let playing = false;
+let score;
+let correctAnswer;
+
+//script upon click start/reset
+document.getElementById("startreset").onclick = ()=> {
+    if(playing){
+        Location.reload();
+    }else{
+        score = 0;
+        document.getElementById("scorevalue").innerHTML = score;
+        show('timeremaining');  //I'm not sure I completely understand this line
+        timeremaining = 60;
+        document.getElementById('timeremainingvalue').innerHTML = timeremaining;
+        document.getElementById('startreset').innerHTML = "Reset Game";
+        playing = true;
+        hide('gameover');
+
+        startCountdown();
+
+        generateQA();
+    }
+}
+
+
+//clicking on an answer choice
+for(i=1; i<5; i++){
+    document.getElementById("box"+i).onclick = ()=> {
+        if(playing == true){
+            if(this.innerHTML == correctAnswer){
+                score++;
+                document.getElementById("scorevalue").innerHTML = score;
+                hide("wrong");
+                show("correct");
+                //setTimeout( ()=> {hide("correct");}, 2000 ); //might be source of bug
+                // setTimeout( hide("correct"), 1000);  //would this also work?
+                setTimeout( function(){
+                    hide("correct");
+                }, 1000);
+                generateQA();
+            }else{
+                hide("correct");
+                show("wrong");
+                setTimeout( ()=> {hide("wrong");}, 1000);
+            }
+        }
+    }
+}
+
+//FUNCTION DEFINITIONS:
+//start counter
+const startCountdown = ()=> {
+    action = setInterval( ()=> {
+        timeremaining -= 1;
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+        if(timeremaining == -1){
+            stopCountdown();
+        }
+    }, 1000);
+}
+
+//stop counter
+const stopCountdown = ()=> {
+    clearInterval(action);
+    show("gameover");
+    document.getElementById("gameover").innerHTML = "<p>game over..!</p><p>your score is " + score + "</p>";
+    hide("timeremaining");
+    hide("correct");
+    hide("wrong");
+    playing = false;
+    document.getElementById("startreset").innerHTML = "Start Game";
+}
+
+//hide element by id
+const hide = (id)=> {
+    document.getElementById(id).style.display = "none";
+}
+//show element by id
+const show = (id)=> {
+    document.getElementById(id).style.display = "block";
+}
+
+
+
+
+
+//random number generator
+const randNum = (n)=> {
+    return Math.floor(Math.random()*n + 1);
+}
+
+//generate question and answers
+const generateQA = ()=> {
+    const operations = ['+', '-', 'x', '/', '^', 'root of'];
+
+    // let x = Math.floor(Math.random()*10 + 1); //note: this might need adjustment
+    // let y = Math.floor(Math.random()*10 + 1); //note: this might need adjustment
+    let x = randNum(10);
+    let y = randNum(10);
+    let rand = Math.floor(Math.random()*6); //note: this too might need adjustment
+    console.log("OPERATION for correct answer: " + rand);
+    console.log("BEFORE: x= " + x + ", and y=" + y);                
+
+    let correctAnswer;
+    switch(rand){
+        case 0: 
+            correctAnswer = x + y;
+            break;
+        case 1: 
+            correctAnswer = x - y;
+            break;
+        case 2: 
+            correctAnswer = x * y;
+            break;
+        case 3: 
+            correctAnswer = x/y;
+            break;
+        case 4: 
+            correctAnswer = x ** y;
+            break;
+        case 5: 
+            correctAnswer = x ** (1/y);
+            break;
+    }
+
+    correctAnswer = correctAnswer.toFixed(2);
+
+    console.log("OPERATION for correct answer: " + rand);
+    console.log("correct Answer: " + correctAnswer);
+    console.log("AFTER: x= " + x + ", and y=" + y);
+
+
+    //post question to the question div
+    document.getElementById("question").innerHTML = rand == 5 ? `${y} ${operations[rand]} ${x}` : `${x} ${operations[rand]} ${y}`;    //might need adjustment
+
+    //generate a 'index position' for the correct answer in the answers array
+    let correctPosition = Math.floor(Math.random()*3 + 1);    //might need adjustment
+    console.log("correct position: " + correctPosition);
+
+    //place answer in random answer box
+    document.getElementById('box'+correctPosition).innerHTML = correctAnswer;
+
+    //generate array of possible answers (including actual answer)
+    let answers = [correctAnswer];      //QUESTION: why is it necessary to include correct answer in array
+
+    //generate wrong answers
+    for(i=1; i<5; i++){
+        if(i != correctPosition){
+            let wrongAnswer;
+            x = randNum(10);
+            y = randNum(10);
+            do{
+                console.log("OPERATION for wrong answer: " + rand + ", x=" + x + ", and y=" + y);
+                switch(rand){
+                    case 0: 
+                        wrongAnswer = x + y;
+                        console.log('addition' + x + "+" + y);
+                        break;
+                    case 1: 
+                        wrongAnswer = x - y;
+                        console.log('subtraction' + x + "-" + y);
+                        break;
+                    case 2: 
+                        wrongAnswer = x * y;
+                        console.log('multiplication' + x + "x" + y);
+                        break;
+                    case 3: 
+                        wrongAnswer = x / y;
+                        console.log('division' + x + "/" + y);
+                        break;
+                    case 4: 
+                        wrongAnswer = x ** y;
+                        console.log('exponential' + x + "^" + y);
+                        break;
+                    case 5: 
+                        wrongAnswer = x ** 1/y;
+                        console.log('root' + x + " root of " + y);
+                        break;
+                }
+            }while(answers.indexOf(wrongAnswer) > -1);   //check that all answers are unique
+
+            console.log(wrongAnswer);
+            wrongAnswer = wrongAnswer.toFixed(2);
+
+            console.log(wrongAnswer);
+            document.getElementById("box"+i).innerHTML = wrongAnswer;
+
+            answers.push(wrongAnswer);
+        }
+    }
+
+    
+
+
+}
